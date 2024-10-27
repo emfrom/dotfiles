5e0b224d948e56858335890079202c5dec70c717
emfrom <vertlaforet@protonmail.com>
2024-10-27 22:44:19 +0100

    Comments updated

--- tangle-all.sh
+++ tangle-all.sh
@@ -2,8 +2,11 @@
 
 #####
 #
-# Auto-tangle script for dotfiles
+# Auto-tangle script for org files
 #
+# Use for convenience or as a git hook
+#
+
 
 # Check for emacs
 if ! command -v emacs &> /dev/null ; then
@@ -11,30 +14,31 @@ if ! command -v emacs &> /dev/null ; then
     exit 0
 fi
 
+
 # Check for git
 if ! command -v git &> /dev/null ; then
     echo "git needed for this script to run"
     exit 0
 fi
 
+
 # Check if in a Git repository
 if ! git rev-parse --is-inside-work-tree &> /dev/null ; then
     echo "Not in a git repository"
-    echo "to continue run:"
-    echo "git clone https://github.com/emfrom/dotfiles.git"
     exit 0
 fi
 
-# Find all org mode files
+
+# Find all org files in the repo
 org_files=$(git ls-files '*.org')
 
-# Check if any .org files were found
+
+# Check if any org files were found
 if [ -z "$org_files" ]; then
-    echo "No .org files found in the repository."
-    echo "to continue run:"
-    echo "git clone https://github.com/emfrom/dotfiles.git"
+    echo "No .org files found in the repo"
     exit 0
 fi
+
 
 # Construct a lisp snippet for emacs to run
 elisp_code="(progn (require 'org) "
@@ -43,11 +47,12 @@ for file in $org_files; do
 done
 elisp_code="$elisp_code)"
 
-# Run the Emacs command with the constructed Elisp code
+
+# Run the code snippet in emacs
 # Note: Doing this without user init files as that seems to screw up the process
 if ! emacs --batch --eval "$elisp_code" 2>&1 | grep "Tangled" ; then
     echo -e "\nSomething went wrong"
-    echo "In all probability org-mode is not installed in your copy of emacs"
+    echo "In all probability org-mode is not installed properly in emacs"
 else
     echo -e "\nAll done"
 fi
